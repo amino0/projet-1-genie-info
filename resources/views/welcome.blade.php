35760f509c2b17ed925d92d301925670eddef421
Amin Ibrahim Kayad <36471537+amino0@users.noreply.github.com>
2024-03-25 20:52:08 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projet GI</title>
+</head>
+<body>
+    <center><h1>Projet 1</h1></center>
+
+<br>
+<br>
+<label for=""> Aller à la page 2 </label> 
+<a href="{{url("https://facebook.com")}}"> Click ici </a>
+    
+<br><br>
+<label for="">Aller a la page 3</label>
+<a href="{{url('/bonsoir')}}">click ici</a>
+</body>
+</html>
